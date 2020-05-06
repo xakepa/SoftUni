82bcd57fd93afab2f7873383cdfe18b031c437d1
xakepa <plamen_ivanov@mail.com>
2020-05-06 18:30:49 +0300

    cats handler added !

---
+++ JS WEB/Node JS/Cat Shelter/resources/handlers/cats.js
@@ -0,0 +1,7 @@
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const qs = require('querystring');
+const formidable = require('formidable');
+const cats = require('../data/cats');
+const breeds = require('../data/breeds');
