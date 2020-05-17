8b10ab897ab7ab2b94e9c74cc4c4c2720b4d2e33
xakepa <plamen_ivanov@mail.com>
2020-05-17 19:38:50 +0300

    Test Express

---
+++ JS WEB/ExpressJS/myDemoApp/routes/index.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+let index = require('../controllers/index');
+/* GET home page. */
+router.get('/', index.index)
+
+module.exports = router;
