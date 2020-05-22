67c0c74bca999be5fd20c6123af6c621b4b789d4
xakepa <plamen_ivanov@mail.com>
2020-05-22 12:55:59 +0300

    Export readHtml function to separate module

--- JS WEB/Node JS/Cat Shelter/handlers/cat.js
+++ JS WEB/Node JS/Cat Shelter/handlers/cat.js
@@ -1,51 +1,53 @@
-// const url = require('url');
-// const fs = require('fs');
-// const path = require('path');
-// const qs = require('querystring');
-// const breeds = require('../database/breeds.json');
-// const cats = require('../database/cats.json');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const qs = require('querystring');
+const breeds = require('../database/breeds.json');
+const cats = require('../database/cats.json');
 
-// module.exports = ((req, res) => {
-//     const pathname = url.parse(req.url).pathname;
-//     let filePath;
+// HANDLER NOT WORKING...
 
-//     if (pathname === '/cats/add-cat' && req.method === 'GET') {
-//         filePath = path.normalize(path.join(__dirname, '../views/addCat.html'));
+module.exports = ((req, res) => {
+    const pathname = url.parse(req.url).pathname;
+    let filePath;
 
-//         fs.readFile(filePath, (err, data) => {
-//             if (err) {
-//                 console.log(err);
+    if (pathname === '/cats/add-cat' && req.method === 'GET') {
+        filePath = path.normalize(path.join(__dirname, '../views/addCat.html'));
 
-//                 res.writeHead(404, {
-//                     'Content-Type': 'text/plain'
-//                 });
-//                 res.write('Page not found!');
-//                 res.end();
-//                 return;
-//             }
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                console.log(err);
 
-//             res.writeHead(200, {
-//                 'Content-Type': 'text/html'
-//             })
-//             res.write(data);
-//             res.end();
-//         })
+                res.writeHead(404, {
+                    'Content-Type': 'text/plain'
+                });
+                res.write('Page not found!');
+                res.end();
+                return;
+            }
 
-//     } else if (pathname === '/cats/add-breed' && req.method === 'GET') {
-//         filePath = path.normalize(path.join(__dirname, '../views/addBreed.html'));
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            })
+            res.write(data);
+            res.end();
+        })
 
-//         const index = fs.createReadStream(filePath);
+    } else if (pathname === '/cats/add-breed' && req.method === 'GET') {
+        filePath = path.normalize(path.join(__dirname, '../views/addBreed.html'));
 
-//         index.on('data', (data) => {
-//             res.write(data);
-//         });
+        const index = fs.createReadStream(filePath);
 
-//         index.on('end', () => res.end());
+        index.on('data', (data) => {
+            res.write(data);
+        });
 
-//         index.on('error', (err) => {
-//             console.log(err);
-//         });
-//     } else {
-//         return true;
-//     }
-// })
+        index.on('end', () => res.end());
+
+        index.on('error', (err) => {
+            console.log(err);
+        });
+    } else {
+        return true;
+    }
+})
