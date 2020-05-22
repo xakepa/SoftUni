7f9492d078ab9ec08a0645b1778b450d32335545
xakepa <plamen_ivanov@mail.com>
2020-05-22 22:28:48 +0300

    test purposes

--- JS WEB/Node JS/Cat Shelter/handlers/cat.js
+++ JS WEB/Node JS/Cat Shelter/handlers/cat.js
@@ -4,6 +4,7 @@ const path = require('path');
 const qs = require('querystring');
 const breeds = require('../database/breeds.json');
 const cats = require('../database/cats.json');
+const readHtml = require('./readHtml');
 
 // HANDLER NOT WORKING...
 
@@ -14,39 +15,26 @@ module.exports = ((req, res) => {
     if (pathname === '/cats/add-cat' && req.method === 'GET') {
         filePath = path.normalize(path.join(__dirname, '../views/addCat.html'));
 
-        fs.readFile(filePath, (err, data) => {
-            if (err) {
-                console.log(err);
-
-                res.writeHead(404, {
-                    'Content-Type': 'text/plain'
-                });
-                res.write('Page not found!');
-                res.end();
-                return;
-            }
-
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            })
-            res.write(data);
-            res.end();
-        })
+        readHtml(filePath, res);
 
     } else if (pathname === '/cats/add-breed' && req.method === 'GET') {
         filePath = path.normalize(path.join(__dirname, '../views/addBreed.html'));
 
-        const index = fs.createReadStream(filePath);
+        readHtml(filePath, res);
 
-        index.on('data', (data) => {
-            res.write(data);
-        });
+        // alternative with Streams 
 
-        index.on('end', () => res.end());
+        // const index = fs.createReadStream(filePath);
 
-        index.on('error', (err) => {
-            console.log(err);
-        });
+        // index.on('data', (data) => {
+        //     res.write(data);
+        // });
+
+        // index.on('end', () => res.end());
+
+        // index.on('error', (err) => {
+        //     console.log(err);
+        // });
     } else {
         return true;
     }
--- JS WEB/Node JS/Cat Shelter/handlers/home.js
+++ JS WEB/Node JS/Cat Shelter/handlers/home.js
@@ -1,9 +1,9 @@
 const url = require('url');
-const fs = require('fs');
 const path = require('path');
 const cats = require('../database/cats.json');
 const breeds = require('../database/breeds.json');
 const http = require('http');
+const readHtml = require('./readHtml');
 
 module.exports = (req, res) => {
     const pathname = url.parse(req.url).pathname;
@@ -14,60 +14,13 @@ module.exports = (req, res) => {
             path.join(__dirname, '../views/home/index.html')
         );
 
-        fs.readFile(filePath, (err, data) => {
-            if (err) {
-                console.log(err);
+        readHtml(filePath, res);
 
-                res.writeHead(404, {
-                    'Content-Type': 'text/plain'
-                });
-                res.write('Page not found!');
-                res.end();
-                return;
-            }
-
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            })
-            res.write(data);
-            res.end();
-        })
     } else if (pathname === '/cats/add-cat' && req.method === 'GET') {
         filePath = path.normalize(path.join(__dirname, '../views/addCat.html'));
 
-        fs.readFile(filePath, (err, data) => {
-            if (err) {
-                console.log(err);
+        readHtml(filePath, res);
 
-                res.writeHead(404, {
-                    'Content-Type': 'text/plain'
-                });
-                res.write('Page not found!');
-                res.end();
-                return;
-            }
-
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            })
-            res.write(data);
-            res.end();
-        })
-
-    } else if (pathname === '/cats/add-breed' && req.method === 'GET') {
-        filePath = path.normalize(path.join(__dirname, '../views/addBreed.html'));
-
-        const index = fs.createReadStream(filePath);
-
-        index.on('data', (data) => {
-            res.write(data);
-        });
-
-        index.on('end', () => res.end());
-
-        index.on('error', (err) => {
-            console.log(err);
-        });
     } else {
         return true;
     }
