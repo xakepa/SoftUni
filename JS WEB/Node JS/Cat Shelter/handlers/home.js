f485de7c71b0ddb56cf25967d232f1ed0c58cffa
xakepa <plamen_ivanov@mail.com>
2020-05-25 18:39:51 +0300

    Rendering new cats

--- JS WEB/Node JS/Cat Shelter/handlers/home.js
+++ JS WEB/Node JS/Cat Shelter/handlers/home.js
@@ -1,5 +1,6 @@
 const url = require('url');
 const path = require('path');
+const fs = require('fs');
 const cats = require('../database/cats.json');
 const breeds = require('../database/breeds.json');
 const http = require('http');
@@ -14,7 +15,32 @@ module.exports = (req, res) => {
             path.join(__dirname, '../views/home/index.html')
         );
 
-        readHtml(filePath, res);
+        const index = fs.createReadStream(filePath);
+
+        index.on('data', (data) => {
+
+            const modifiedCats = cats.map(cat => `
+            <li>
+            <img src="${path.join('./content/images' + cat.image)}" alt="${cat.name}">
+                    <h3>${cat.name}</h3>
+                    <p><span>Breed: </span>${cat.breed}</p>
+                    <p><span>Description: </span>${cat.description}</p>
+                    <ul class="buttons">
+                        <li class="btn edit"><a href="/cats-edit/${cat.id}">Change Info</a></li>
+                        <li class="btn delete"><a href="/cats-find-new-home/${cat.id}">New Home</a></li>
+                    </ul>
+                    </li>`);
+
+            const modifiedData = String(data).replace('{{cats}}', modifiedCats);
+
+            res.write(modifiedData);
+        });
+
+        index.on('end', () => res.end());
+
+        index.on('error', (err) => {
+            console.log(err);
+        });
 
     } else {
         return true;
