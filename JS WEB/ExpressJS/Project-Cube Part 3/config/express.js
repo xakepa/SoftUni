9c0bb5150529ed7e1dde871bf92446fac914a2e3
xakepa <plamen_ivanov@mail.com>
2020-06-18 20:29:44 +0300

    refactor

--- JS WEB/ExpressJS/Project-Cube Part 3/config/express.js
+++ JS WEB/ExpressJS/Project-Cube Part 3/config/express.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+
 
 module.exports = (app) => {
-
+    app.use(cookieParser());
+    app.use(express.json())
     app.engine('.hbs', handlebars({
         extname: '.hbs'
     }));
