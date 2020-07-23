c64cccc20d10370891f628cc07ac68d1f1e26c6c
xakepa <plamen_ivanov@mail.com>
2020-07-23 14:12:31 +0300

    links changed

--- JS WEB/REACT JS/Origami Workshop/origami/src/components/header/index.js
+++ JS WEB/REACT JS/Origami Workshop/origami/src/components/header/index.js
@@ -2,23 +2,25 @@ import React from 'react';
 import styles from './index.module.css';
 import Link from '../link';
 import logo from '../../images/white-origami-bird.png';
+import getNavigation from '../../utils/navigation';
 
 const Header = () => {
+    const links = getNavigation();
+
     return (
         <nav className={styles.navigation}>
             <ul>
                 <img className={styles.logo} src={logo} alt="Logo" />
-                <Link href={"#"} number={1} type="header" />
-                <Link href={"#"} number={2} type="header" />
-                <Link href={"#"} number={3} type="header" />
-                <Link href={"#"} number={4} type="header" />
-                <Link href={"#"} number={5} type="header" />
-                <Link href={"#"} number={6} type="header" />
-                <Link href={"#"} number={7} type="header" />
-                <Link href={"#"} number={8} type="header" />
-                <Link href={"#"} number={9} type="header" />
-                <Link href={"#"} number={10} type="header" />
-                <Link href={"#"} number={11} type="header" />
+                {
+                    links.map(l => <Link href={l.link} value={l.title} type="header" />)
+                }
+                <Link href={"#"} value={'#######'} type="header" />
+                <Link href={"#"} value={'#######'} type="header" />
+                <Link href={"#"} value={'#######'} type="header" />
+                <Link href={"#"} value={'#######'} type="header" />
+                <Link href={"#"} value={'#######'} type="header" />
+                <Link href={"#"} value={'#######'} type="header" />
+                <Link href={"#"} value={'#######'} type="header" />
             </ul>
         </nav>
     )
--- JS WEB/REACT JS/Origami Workshop/origami/src/components/link/index.js
+++ JS WEB/REACT JS/Origami Workshop/origami/src/components/link/index.js
@@ -4,7 +4,7 @@ import styles from './index.module.css';
 const Link = (props) => {
     return (
         <div className={styles[`${props.type}-list-item`]}>
-            <a className={styles[`${props.type}-link`]} href={props.href} >Going to {props.number}</a>
+            <a className={styles[`${props.type}-link`]} href={props.href} >{props.value}</a>
         </div>
     )
 }
